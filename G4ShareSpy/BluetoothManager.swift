c9f7c0f1774ddc6880c7ccce6624cd0987c832dd
Pete Schwamb <pete@schwamb.net>
2019-04-02 07:19:33 -0500

    Fix compilation warnings, remove queue QOS (#3)

--- G4ShareSpy/BluetoothManager.swift
+++ G4ShareSpy/BluetoothManager.swift
@@ -56,7 +56,7 @@ class BluetoothManager: NSObject, CBCentralManagerDelegate, CBPeripheralDelegate
 
     // MARK: - GCD Management
 
-    private var managerQueue = DispatchQueue(label: "com.warkmilson.G4ShareSpy.bluetoothManagerQueue", qos: .userInitiated)
+    private var managerQueue = DispatchQueue(label: "com.warkmilson.G4ShareSpy.bluetoothManagerQueue", qos: .unspecified)
 
     override init() {
         super.init()
@@ -102,7 +102,7 @@ class BluetoothManager: NSObject, CBCentralManagerDelegate, CBPeripheralDelegate
             peripheral.delegate = self
             manager.connect(peripheral, options: nil)
         } else {
-            DispatchQueue.global(qos: DispatchQoS.QoSClass.utility).async {
+            DispatchQueue.global(qos: .utility).async {
                 Thread.sleep(forTimeInterval: 10)
                 self.findConnectedReceiver(withRetries: retries - 1)
             }
